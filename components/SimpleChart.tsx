8c06656c438ee4c842c47d418e244e983874475b
mkotsollaris <mkotsollari@gmail.com>
2023-02-05 15:09:57 -0800

    borderWitdth

--- components/SimpleChart.tsx
+++ components/SimpleChart.tsx
@@ -25,7 +25,7 @@ const data = {
       data: reactionRate,
       backgroundColor: 'rgba(255, 99, 132, 0.2)',
       borderColor: 'rgba(255, 99, 132, 1)',
-      borderWidth: 1,
+      borderWidth: 3,
       fill: false,
     },
   ],
